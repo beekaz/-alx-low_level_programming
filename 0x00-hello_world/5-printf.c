366ebe7d0a488499772ed2942e7902615acd1b15
beekaz <sooperfect00@gmail.com>
2023-02-16 16:24:20 -0800

    a C program that prints the size of various types on the computer it is compiled and run on.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- * main-Entry point
- *
- *Return: Always 0 succes
- */
+* main- A program that prints using prinf function
+* Return: Always 0 (Sucess)
+*/
 
 int main(void)
 {
-	prinf("\"with proper grammar, but the outcome is a piece of art")
-		return (0);
+printf("with proper grammar,but the outcome is a piece of art,\n");
+return (0);
 }
