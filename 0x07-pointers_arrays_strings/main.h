f9e9544a495ab87f2ae5f293231a6a509e5f3693
beekaz <sooperfect00@gmail.com>
2023-03-07 16:03:53 -0800

    fixing bugs

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -10,4 +10,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
  void print_chessboard(char (*a)[8]);
  void print_diagsums(int *a, int size);
 int _putchar(char c);
+void set_string(char **s, char *to);
+
 #endif
